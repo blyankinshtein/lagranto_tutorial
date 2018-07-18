da77009e56f77b20866bac30fc657abc565cea99
natalia bliankinshtein <blyankinshtein@gmail.com>
2018-07-18 17:04:01 -0400

    comments

--- README.md
+++ README.md
@@ -3,22 +3,28 @@ A detailed example of lagranto.ecmwf use for identifying warm conveyor belts.
 
 Based on Lagranto Documentation (Release 0.1) by Nicolas Piaget available at https://media.readthedocs.org/pdf/lagranto/latest/lagranto.pdf and Lagranto Tutorial by Michael Sprenger and Heini Wernli available at http://iacweb.ethz.ch/staff//sprenger/lagranto/tutorial/Lagranto-Tutorial.pdf.
 
+In this tutorial, we find warm conveyor belts over the Atlantic and Europe on June 1-5, 2013.
+
 This repository includes scripts for:
 
 1) downloading ERA Interim data from ECMWF (ecmwf_download.py), 
-2) processing them with NCO, 
-3) running LAGRANTO as a shell script and 
-% 4) plotting the results in Jupyter Notebook. 
+2) processing them with NCO to meet LAGRANTO format requirements (ecmwf2nc_nco_script.sh), 
+3) running LAGRANTO as a shell script (lagrantodata/run_lagranto.sh) and 
+4) plotting the results in a Jupyter Notebook (Plotting_trajectories.ipynb). 
 
 # Instructions:
 mkdir erai
 
-python ecmwf_download.py % in background: nohup python ecmwf_download.py > out.txt & 
+python ecmwf_download.py % or, in background: nohup python ecmwf_download.py > out.txt & 
 
 sh ecmwf2nc_nco_script.sh
 
 cd lagrantodata
 
-sh rul_lagranto.sh
+sh run_lagranto.sh
 
+cd ..
 
+jupyter notebook
+
+Then execute the code in Plotting_trajectories.ipynb.
--- lagrantodata/run_lagranto.sh
+++ lagrantodata/run_lagranto.sh
@@ -1,14 +1,23 @@
 #!/bin/sh
 
+# launch trajectories from 00UTC of June 1, 2013 to 00UTC of June 3, 2013
+
 startdate='20130601_00'
 enddate='20130603_00'
 
-startdates=('20130601_06' '20130601_12' '20130601_18' '20130602_00' '20130602_06' '20130602_12' '20130602_18' '20130603_00')
-enddates=('20130603_06' '20130603_12' '20130603_18' '20130604_00' '20130604_06' '20130604_12' '20130604_18' '20130605_00')
+# create a file with starting locations
+# every 80 km in a box [60W, 20E, 30N, 80N]
+# every 8 hPa in a layer 1030-790 hPa
 
 create_startf $startdate startf.2 'box.eqd(-60,20,30,80,80)@profile(1030,790,8)@hPa' -changet
 
+# calculate trajectories. -j flag prevents stops at the ground level
 caltra $startdate $enddate startf.2 traj.4 -j   
+
+#repeat for additional trajectories launched every 6 hours
+
+startdates=('20130601_06' '20130601_12' '20130601_18' '20130602_00' '20130602_06' '20130602_12' '20130602_18' '20130603_00')
+enddates=('20130603_06' '20130603_12' '20130603_18' '20130604_00' '20130604_06' '20130604_12' '20130604_18' '20130605_00')
 
 for i in {0..7};
 do
@@ -20,5 +29,10 @@ caltra ${startdates[i]} ${enddates[i]} startf.2 traj_i.4 -j
 mergetra traj_i.4 traj.4 traj.4
 done
 
+# select trajectories with ascent of more than 500 hPa
+
 select.ecmwf traj.4 wcb.1 'GT:p(DIFF):500:FIRST,LAST'
+
+# trace variables from tracevars file along selected trajectories
+
 trace wcb.1 wcb.1
