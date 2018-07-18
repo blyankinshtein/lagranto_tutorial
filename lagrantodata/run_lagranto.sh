795303eb9eac85d0be0342e698d155f28e68bce1
natalia bliankinshtein <blyankinshtein@gmail.com>
2018-07-18 16:18:22 -0400

    a good example of WCB detection

--- lagrantodata/run_lagranto.sh
+++ lagrantodata/run_lagranto.sh
@@ -1,11 +1,24 @@
 #!/bin/sh
 
 startdate='20130601_00'
-enddate='20130604_00'
+enddate='20130603_00'
 
-create_startf $startdate startf.2 'box.eqd(-80,20,40,80,80)@ level(100)@hPa,agl' -changet
-head -10 startf.2
+startdates=('20130601_06' '20130601_12' '20130601_18' '20130602_00' '20130602_06' '20130602_12' '20130602_18' '20130603_00')
+enddates=('20130603_06' '20130603_12' '20130603_18' '20130604_00' '20130604_06' '20130604_12' '20130604_18' '20130605_00')
+
+create_startf $startdate startf.2 'box.eqd(-60,20,30,80,80)@profile(1030,790,8)@hPa' -changet
+
 caltra $startdate $enddate startf.2 traj.4 -j   
-select.ecmwf traj.4 wcb.1 'GT:p:700:FIRST & LT:p(MIN):400:0 to 48'
-trace wcb.1 wcb.1
 
+for i in {0..7};
+do
+#echo $i
+#echo ${startdates[i]}
+create_startf ${startdates[i]} startf.2 'box.eqd(-60,20,30,80,80)@profile(1030,790,8)@hPa' -changet
+caltra ${startdates[i]} ${enddates[i]} startf.2 traj_i.4 -j   
+
+mergetra traj_i.4 traj.4 traj.4
+done
+
+select.ecmwf traj.4 wcb.1 'GT:p(DIFF):500:FIRST,LAST'
+trace wcb.1 wcb.1
